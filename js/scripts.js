ef9cf1b9f4536520478de83ac75f485711b856ec
Tiffany Siu <tsiu88@gmail.com>
2020-01-15 13:26:27 -0800

    add initial html, js, and css for form input

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(){
+    var nameInput = $("#name").val();
+
+    $(".name").append(nameInput);
+
+    $("#postcard").show();
+    event.preventDefault();
+  });
+});
